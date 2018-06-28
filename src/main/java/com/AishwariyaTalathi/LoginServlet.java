bfff4539b5e832431ceb90635a036bcc3e87b29b
AishwariyaTalathi <aishwariyatalathi3106@gmail.com>
2018-06-27 23:53:11 -0700

    First commit

--- src/main/java/com/AishwariyaTalathi/LoginServlet.java
+++ src/main/java/com/AishwariyaTalathi/LoginServlet.java
@@ -9,12 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns="/login.do")
+@WebServlet(urlPatterns="/logins.do")
 public class LoginServlet extends HttpServlet {
 	 protected void doGet(HttpServletRequest request,HttpServletResponse response) 
 			 throws ServletException, IOException {
 		//Redirecting request from this servlet to JSP by giving 
 		// The path of the JSP file in the requestdispatcher
+		  
+		 request.setAttribute("name", request.getParameter("name"));
+		 request.setAttribute("University", request.getParameter("University"));
+		 request.setAttribute("Major", request.getParameter("Major"));
 		 request.getRequestDispatcher("/WEB-INF/Views/login.jsp").forward(request, response);
 	}
 	  
--- src/main/webapp/WEB-INF/Views/login.jsp
+++ src/main/webapp/WEB-INF/Views/login.jsp
@@ -1,3 +1,5 @@
+
+<%@page import="java.util.Date"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
@@ -7,6 +9,18 @@
 <title>MyPage From JSP</title>
 </head>
 <body>
-My First JSP
+<%
+Date date = new Date();
+%>
+
+<div>Current Date is <%=date%></div>
+<div>My Name is ${name}</div>
+<div>I successfully graduated from ${University} and earned my masters in the field of ${Major}<div>
 </body>
 </html>
+
+<%//Code to depict use of scriplets%>
+<%//Pass Input as http://localhost:8080/?name=Aishwariya&University=CSULB&Major=ComputerScience %>
+<%//Output on browser is:%>
+<%//Current Date is Wed Jun 27 23:36:44 PDT 2018%>
+<%//I successfully graduated from CSULB and earned my masters in the field of ComputerScience%> 
